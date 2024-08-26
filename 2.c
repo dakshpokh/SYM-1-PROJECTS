a7b11da961b007c7c0091ad3dff5e2ae059e3cb8
dakshpokh <itisdakshpokhriyal@gmail.com>
2024-08-26 12:32:25 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{   int a,b,c,d,e,f;
+
+    a=10;
+    b=5;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+
+    printf("sum is %d:\n ",a);
+    printf(" subtraction is %d:\n ",b);
+    printf("product is %d\n",e);
+    printf("division is %d\n",f);
+
+
+
+
+
+
+    return 0;
+}
